3a54ff07323d729e99fa879d72131aa06b46d3b4
ermaerman <ermafel@gmail.com>
2018-05-10 23:04:22 +0700

    eighth commit

---
+++ config/add-nisn.php
@@ -0,0 +1,68 @@
+<?php
+
+	error_reporting(0);
+    
+    include 'koneksi.php';
+
+    $id_data_calon_murid = $_POST['id_data_calon_murid'];
+	$kode_daftar    = $_POST['kode_daftar'];
+
+    $target_dir = "../nisn/";
+    $target_file = $target_dir . basename($_FILES["fileToUploadNisn"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    
+    // Check if image file is a actual image or fake image
+    if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUploadNisn"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+    
+    // Check file size
+    if ($_FILES["fileToUploadNisn"]["size"] > 50000000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" && $imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG"
+    && $imageFileType != "GIF" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["fileToUploadNisn"]["tmp_name"], $target_file)) {
+            echo "The file ". basename( $_FILES["fileToUploadNisn"]["name"]). " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+
+	$update 	= "UPDATE tbl_calon_data_murid SET syarat_nisn='$target_file' WHERE kode_daftar='$kode_daftar'";
+	$updatenisn	= mysqli_query($konek, $update)or die(mysqli_error());
+
+	if ($updatenisn)
+    	{
+    		echo "<br><br><br><strong><center><i>Berhasil upload NISN!";
+    		echo '<META HTTP-EQUIV="REFRESH" CONTENT = "1; URL=../index.php?content=upload-file">';
+    	}
+	else {
+    		print"
+    			<script>
+    				alert(\"Balasan gagal ditambah!\");
+    				history.back(-1);
+    			</script>";
+    	}
+?>
--- upload-file.php
+++ upload-file.php
@@ -31,10 +31,11 @@
             <label class="col-sm-2">Kode Daftar</label>
             <label class="col-sm-1">:</label>
             <div class="col-sm-3">
-                <input onkeypress="return hanyaAngka(event)" class="form-control" name="kode_daftar" type="text" placeholder="Masukkan kode daftar anda" required>
+                <input onkeypress="return hanyaAngka(event)" class="form-control" name="kode_daftar" type="text" placeholder="Masukkan kode daftar anda">
             </div>
             <button class="btn btn-danger" type="submit" value="pencarian" name="cek">Cek Kekurangan File</button>
         </div>
+      </form>
         <br>
         <br>
         <br>
@@ -253,12 +254,245 @@
         <br>
         <br>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;
-        <button type="button" class="btn btn-danger">NISN</button>
-        <button type="button" class="btn btn-danger">Ijazah</button>
-        <button type="button" class="btn btn-danger">Kartu Keluarga</button>
-        <button type="button" class="btn btn-danger">Akte Kelahiran</button>
-        <button type="button" class="btn btn-danger">Foto 3x4</button>
+        <button data-toggle="modal" data-target="#myModal1" type="button" class="btn btn-danger">NISN</button>
+        <button data-toggle="modal" data-target="#myModal2" type="button" class="btn btn-danger">Ijazah</button>
+        <button data-toggle="modal" data-target="#myModal3" type="button" class="btn btn-danger">Kartu Keluarga</button>
+        <button data-toggle="modal" data-target="#myModal4" type="button" class="btn btn-danger">Akte Kelahiran</button>
+        <button data-toggle="modal" data-target="#myModal5" type="button" class="btn btn-danger">Foto 3x4</button>
         </p>
+
+        <!-- Modal NISN -->
+<div id="myModal1" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header" style="background-color:#db4e66";>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <p align="center"><img src="gambar/manda.png" alt="" height="100px" width="130px"></p>
+            <p align="center"><font size="2px"><i>Sistem Informasi Pendaftaran SMK Mandalahayu II Bekasi</i></font></p>
+            <h4 class="modal-title" align="center"><b>Upload kekurangan NISN</b></h4>
+          </div>
+          <br>
+          <div class="modal-body">
+            <form action="config/add-nisn.php" class="form-horizontal" method="POST" enctype="multipart/form-data">
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Kode Daftar</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control" name="kode_daftar" placeholder="Kode Daftar" required>
+                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Upload NISN</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="file" name="fileToUploadNisn" id="fileToUploadNisn">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label class="control-label col-sm-4"></label>
+                  <div class="col-sm-6" align="right">
+                      <button type="submit" class="btn btn-danger">Upload</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+      </div>
+    </div>
 </div>
+
+        <!-- Modal Ijazah -->
+<div id="myModal2" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header" style="background-color:#db4e66";>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <p align="center"><img src="gambar/manda.png" alt="" height="100px" width="130px"></p>
+            <p align="center"><font size="2px"><i>Sistem Informasi Pendaftaran SMK Mandalahayu II Bekasi</i></font></p>
+            <h4 class="modal-title" align="center"><b>Upload kekurangan Ijazah</b></h4>
+          </div>
+          <br>
+          <div class="modal-body">
+            <form action="../config/add-ijazah.php" class="form-horizontal" method="POST">
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Kode Daftar</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control" name="kode_daftar" placeholder="Kode Daftar" required>
+                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Upload Ijazah</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="file" name="fileToUploadIjazah" id="fileToUploadIjazah">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label class="control-label col-sm-4"></label>
+                  <div class="col-sm-6" align="right">
+                      <button class="btn btn-danger">Upload</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+      </div>
+    </div>
 </div>
+
+        <!-- Modal Kartu Keluarga -->
+<div id="myModal3" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header" style="background-color:#db4e66";>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <p align="center"><img src="gambar/manda.png" alt="" height="100px" width="130px"></p>
+            <p align="center"><font size="2px"><i>Sistem Informasi Pendaftaran SMK Mandalahayu II Bekasi</i></font></p>
+            <h4 class="modal-title" align="center"><b>Upload kekurangan Kartu Keluarga</b></h4>
+          </div>
+          <br>
+          <div class="modal-body">
+            <form action="../config/add-kk.php" class="form-horizontal" method="POST">
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Kode Daftar</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control" name="kode_daftar" placeholder="Kode Daftar" required>
+                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Upload Kartu Keluarga</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="file" name="fileToUploadKk" id="fileToUploadKk">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label class="control-label col-sm-4"></label>
+                  <div class="col-sm-6" align="right">
+                      <button class="btn btn-danger">Upload</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+      </div>
+    </div>
+</div>
+
+
+        <!-- Modal Akte Kelahiran -->
+<div id="myModal4" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header" style="background-color:#db4e66";>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <p align="center"><img src="gambar/manda.png" alt="" height="100px" width="130px"></p>
+            <p align="center"><font size="2px"><i>Sistem Informasi Pendaftaran SMK Mandalahayu II Bekasi</i></font></p>
+            <h4 class="modal-title" align="center"><b>Upload kekurangan Akte Kelahiran</b></h4>
+          </div>
+          <br>
+          <div class="modal-body">
+            <form action="../config/add-akte.php" class="form-horizontal" method="POST">
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Kode Daftar</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control" name="kode_daftar" placeholder="Kode Daftar" required>
+                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Upload Akte Kelahiran</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="file" name="fileToUploadAkte" id="fileToUploadAkte">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label class="control-label col-sm-4"></label>
+                  <div class="col-sm-6" align="right">
+                      <button class="btn btn-danger">Upload</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+      </div>
+    </div>
+</div>
+        <!-- Modal Foto -->
+<div id="myModal5" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+          <div class="modal-header" style="background-color:#db4e66";>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <p align="center"><img src="gambar/manda.png" alt="" height="100px" width="130px"></p>
+            <p align="center"><font size="2px"><i>Sistem Informasi Pendaftaran SMK Mandalahayu II Bekasi</i></font></p>
+            <h4 class="modal-title" align="center"><b>Upload kekurangan Foto</b></h4>
+          </div>
+          <br>
+          <div class="modal-body">
+            <form action="../config/add-foto.php" class="form-horizontal" method="POST">
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Kode Daftar</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control" name="kode_daftar" placeholder="Kode Daftar" required>
+                  </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2"></label>
+                <label class="col-sm-3">Upload Foto</label>
+                <label class="col-sm-1">:</label>
+                  <div class="col-sm-4">
+                      <input type="file" name="fileToUploadFoto" id="fileToUploadFoto">
+                  </div>
+              </div>
+              <div class="form-group">
+                  <label class="control-label col-sm-4"></label>
+                  <div class="col-sm-6" align="right">
+                      <button class="btn btn-danger">Upload</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            
+          </div>
+      </div>
+    </div>
+</div>
+
+
+</div>
+
+
   

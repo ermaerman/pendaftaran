e47263d09a4314988ef8491185d0c970e455c330
ermaerman <ermafel@gmail.com>
2018-05-25 23:22:02 +0700

    sixteenth commit

--- cek-proses.php
+++ cek-proses.php
@@ -65,40 +65,58 @@
         $data = mysqli_fetch_array($query);
           
           //belum dikonfirmasi admin
-          if ($data['b_daftar']==0 AND $data['validasi_daftar']==0) {
+          if ($data['b_daftar']==0 AND $data['validasi_daftar']==0 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==0 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
               echo "<i>Anda belum membayar biaya pendaftaran.</i><br>";
               echo "<i>Silahkan membayar biaya pendaftaran anda, jika sudah membayar pendaftaran silahkan klik <a data-toggle='tooltip' data-placement='top' title='Konfirmasi Pembayaran' href='index.php?content=konfirmasi-pembayaran'>disini</a> untuk konfirmasi pembayaran.<br> Atau klik <a data-toggle='tooltip' data-placement='bottom' title='Informasi Biaya' href='index.php?content=biaya'>disini</a> untuk melihat informasi biaya.</i>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==0) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==0 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==0 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
               echo "<i>Anda sudah membayar biaya pendaftaran tetapi pembayaran belum dikonfirmasi oleh tata usaha kami.</i><br>";
               echo "<i>Silahkan tunggu beberapa saat lagi sampai dikonfirmasi oleh tatausaha kami. Kemudian cek proses kembali.</i>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==0 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
               echo "<i>Anda sudah membayar biaya pendaftaran dan pembayaran telah dikonfirmasi oleh tata usaha kami.</i><br>";
               echo "<i>Selanjutnya, silahkan lihat <a data-toggle='tooltip' data-placement='bottom' title='Lihat Tanggal Ujian' href='index.php?content=tgl-ujian&&kode_daftar=$kode_daftar'>disini</a> untuk informasi selanjutnya.</i>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil']==0 AND $data['validasi_pangkal_cicil']==0) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==0 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
-              echo "<i>Anda sudah membayar biaya uang pangkal lunas tetapi pembayaran belum dikonfirmasi oleh tata usaha.</i><br>";
-              echo "<i>Silahkan tunggu beberapa saat lagi.</i>";
+              echo "<i>Anda sudah membayar biaya uang pangkal lunas tetapi pembayaran belum dikonfirmasi oleh tata usaha kami.</i><br>";
+              echo "<i>Silahkan tunggu beberapa saat lagi sampai dikonfirmasi oleh tatausaha kami. Kemudian cek proses kembali.</i>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==1 AND $data['b_pangkal_cicil']==0 AND $data['validasi_pangkal_cicil']==0) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==1 AND $data['b_pangkal_cicil1']==0 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
-              echo "<i>Anda sudah membayar biaya uang pangkal lunas dan pembayaran sudah dikonfirmasi oleh tata usaha.</i><br>";
-              echo "<i>Terimakasih karena anda telah melunasi biaya administrasi dan uang pangkal. Untuk informasi selanjutnya silahkan lihat <a href='#'>disini</a>.</i>";
+              echo "<i>Terimakasih, Anda sudah membayar biaya uang pangkal lunas dan pembayaran sudah dikonfirmasi oleh tata usaha kami.</i><br>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==1 AND $data['b_pangkal_cicil']==1 AND $data['validasi_pangkal_cicil']==0) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==0 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
-              echo "<i>Anda sudah membayar biaya cicilan uang pangkal tetapi pembayaran belum dikonfirmasi oleh tata usaha.</i><br>";
-              echo "<i>Silahkan tunggu beberapa saat lagi.</i>";
+              echo "<i>Anda telah membayar biaya uang pangkal cicilan pertama tetapi pembayaran belum dikonfirmasi oleh tatausaha kami.</i><br>";
+              echo "<i>Silahkan tunggu beberapa saat lagi sampai dikonfirmasi oleh tatausaha kami. Kemudian cek proses kembali.</i>";
           }
-          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==1 AND $data['validasi_pangkal_lunas']==1 AND $data['b_pangkal_cicil']==1 AND $data['validasi_pangkal_cicil']==1) {
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==1 AND $data['b_pangkal_cicil2']==0 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
               echo "<h2>Cek Proses</h2><hr>";
-              echo "<i>Anda sudah membayar biaya cicilan uang pangkal dan pembayaran sudah dikonfirmasi oleh tata usaha.</i><br>";
-              echo "<i>Silahkan tunggu beberapa saat lagi.</i>";
+              echo "<i>Anda telah membayar biaya uang pangkal pertama dan  pembayaran telah dikonfirmasi oleh tatausaha kami.</i><br>";
+              echo "<i>Silahkan membayar pembayaran uang pangkal kedua, klik <a data-toggle='tooltip' data-placement='bottom' title='Konfirmasi Pembayaran Uang Pangkal Cicil Kedua' href='index.php?content=konfirmasi-pembayaran'>disini</a> untuk konfirmasi pembayaran.</i>";
+          }
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==1 AND $data['b_pangkal_cicil2']==1 AND $data['validasi_pangkal_cicil2']==0 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
+              echo "<h2>Cek Proses</h2><hr>";
+              echo "<i>Anda telah membayar biaya uang pangkal cicilan kedua tetapi pembayaran belum dikonfirmasi oleh tatausaha kami.</i><br>";
+               echo "<i>Silahkan tunggu beberapa saat lagi sampai dikonfirmasi oleh tatausaha kami. Kemudian cek proses kembali.</i>";
+          }
+          elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==1 AND $data['b_pangkal_cicil2']==1 AND $data['validasi_pangkal_cicil2']==1 AND $data['b_pangkal_cicil3']==0 AND $data['validasi_pangkal_cicil3']==0) {
+              echo "<h2>Cek Proses</h2><hr>";
+              echo "<i>Anda telah membayar biaya uang pangkal cicilan kedua dan pembayaran telah dikonfirmasi oleh tatausaha kami.</i><br>";
+               echo "<i>Silahkan membayar pembayaran uang pangkal ketiga, klik <a data-toggle='tooltip' data-placement='bottom' title='Konfirmasi Pembayaran Uang Pangkal Cicil Ketiga' href='index.php?content=konfirmasi-pembayaran'>disini</a> untuk konfirmasi pembayaran.</i>";
+          }
+           elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==1 AND $data['b_pangkal_cicil2']==1 AND $data['validasi_pangkal_cicil2']==1 AND $data['b_pangkal_cicil3']==1 AND $data['validasi_pangkal_cicil3']==0) {
+              echo "<h2>Cek Proses</h2><hr>";
+              echo "<i>Anda telah membayar biaya uang pangkal cicilan ketiga tetapi pembayaran belum dikonfirmasi oleh tatausaha kami.</i><br>";
+               echo "<i>Silahkan tunggu beberapa saat lagi sampai dikonfirmasi oleh tatausaha kami. Kemudian cek proses kembali.</i>";
+          }
+           elseif ($data['b_daftar']==1 AND $data['validasi_daftar']==1 AND $data['b_pangkal_lunas']==0 AND $data['validasi_pangkal_lunas']==0 AND $data['b_pangkal_cicil1']==1 AND $data['validasi_pangkal_cicil1']==1 AND $data['b_pangkal_cicil2']==1 AND $data['validasi_pangkal_cicil2']==1 AND $data['b_pangkal_cicil3']==1 AND $data['validasi_pangkal_cicil3']==1) {
+              echo "<h2>Cek Proses</h2><hr>";
+              echo "<i>Terimakasih, Anda telah membayar biaya uang pangkal cicilan ketiga dan pembayaran telah dikonfirmasi oleh tatausaha kami. Pembayaran uang pangkal telah dilakukan sebanyak 3 kali cicilan dan telah lunas.</i><br>";
           }
       }
 ?>
